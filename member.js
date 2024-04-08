be28b8864bb63ec011c07aa324c9a10fc921b8d1
kuromami <163090079+kuromami@users.noreply.github.com>
2024-04-08 06:16:59 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember()
+{
+    var member = new Object();
+    member.name = "John";
+    member.age = 25;
+    member.skills = ["C#", "ASP.NET", "SQL Server"];
+    return member;
+}
